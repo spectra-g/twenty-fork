a63c2ee00e43606bb1d2134655d7605a305c5349
spectra-g <gpatelish@gmail.com>
2026-02-25 12:39:19 +0000

    feat(scrum-401): implement SCRUM-401

---
+++ packages/twenty-server/src/modules/task/index.ts
@@ -0,0 +1,3 @@
+export { TaskStatus } from 'src/modules/task/enums/task-status.enum';
+export { TaskPreUpdateHook } from 'src/modules/task/hooks/task-pre-update.hook';
+export { type Task } from 'src/modules/task/interfaces/task.interface';
