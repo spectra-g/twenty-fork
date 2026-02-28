c829fdd98a84a64dbcff5c63208ac33ae7594316
spectra-g <gpatelish@gmail.com>
2026-02-28 21:02:38 +0000

    feat(SCRUM-475): implement SCRUM-475 (#35)

--- packages/twenty-server/src/utils/__test__/is-work-email.spec.ts
+++ packages/twenty-server/src/utils/__test__/is-work-email.spec.ts
@@ -1,4 +1,4 @@
-import { isWorkEmail } from 'src/utils/is-work-email';
+import { isWorkDomain, isWorkEmail } from 'src/utils/is-work-email';
 
 describe('isWorkEmail', () => {
   it('should return true for a work email', () => {
@@ -20,5 +20,19 @@ describe('isWorkEmail', () => {
 
   it('should return false for an invalid email format', () => {
     expect(isWorkEmail('invalid-email')).toBe(false);
+  });
+});
+
+describe('isWorkDomain', () => {
+  it('should return false for uppercase personal domain GMAIL.COM', () => {
+    expect(isWorkDomain('GMAIL.COM')).toBe(false);
+  });
+
+  it('should return false for mixed-case personal domain Yahoo.COM', () => {
+    expect(isWorkDomain('Yahoo.COM')).toBe(false);
+  });
+
+  it('should return true for mixed-case work domain Company.COM', () => {
+    expect(isWorkDomain('Company.COM')).toBe(true);
   });
 });
