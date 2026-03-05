b098433e048918107b57467e8f1aa5b99c81ca2f
spectra-g <gpatelish@gmail.com>
2026-03-05 15:14:23 +0000

    feat(SCRUM-535): implement SCRUM-535

--- packages/twenty-server/src/utils/is-work-email.ts
+++ packages/twenty-server/src/utils/is-work-email.ts
@@ -1,14 +1,18 @@
 import { emailProvidersSet } from 'src/utils/email-providers';
 import { getDomainNameByEmail } from 'src/utils/get-domain-name-by-email';
 
+const normalizedEmailProvidersSet = new Set(
+  Array.from(emailProvidersSet, (provider) => provider.toLowerCase()),
+);
+
 export const isWorkEmail = (email: string) => {
   try {
-    return !emailProvidersSet.has(getDomainNameByEmail(email));
+    return isWorkDomain(getDomainNameByEmail(email));
   } catch {
     return false;
   }
 };
 
 export const isWorkDomain = (domain: string) => {
-  return !emailProvidersSet.has(domain);
+  return !normalizedEmailProvidersSet.has(domain.toLowerCase());
 };
