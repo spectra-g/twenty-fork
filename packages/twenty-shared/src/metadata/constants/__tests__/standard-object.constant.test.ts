03df8edf27f1a773dcb0599c0b8a033ff8cc3ed5
spectra-g <gpatelish@gmail.com>
2026-03-20 15:18:09 +0000

    feat(PROJ-33): implement PROJ-33 (#86)

--- packages/twenty-shared/src/metadata/constants/__tests__/standard-object.constant.test.ts
+++ packages/twenty-shared/src/metadata/constants/__tests__/standard-object.constant.test.ts
@@ -1,9 +1,10 @@
-import { STANDARD_OBJECTS } from '@/metadata/constants/standard-object.constant';
+import { STANDARD_OBJECTS } from '../standard-object.constant';
 
 describe('STANDARD_OBJECTS blocklist', () => {
-  it('should define a description field', () => {
-    expect(STANDARD_OBJECTS.blocklist.fields.description).toEqual({
-      universalIdentifier: expect.any(String),
+  it('should define a stable universal identifier for the description field', () => {
+    expect(STANDARD_OBJECTS.blocklist.fields).toHaveProperty('description');
+    expect(STANDARD_OBJECTS.blocklist.fields.description).toStrictEqual({
+      universalIdentifier: '20202020-1af0-4f77-8e9f-53227b3b6a4b',
     });
   });
 });
